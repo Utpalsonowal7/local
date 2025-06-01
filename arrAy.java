c75681174d21b36dcbfcc30adfa1e0a03c2cd981
Utpal Sonowal <132993823+Utpalsonowal7@users.noreply.github.com>
2025-06-01 14:40:48 +0000

    array

---
+++ arrAy.java
@@ -0,0 +1,11 @@
+public  class arrAy {
+    public static void main(String[] args) {
+        int arr[] = {1, 5, 10, 15};
+
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
