6afd184ca640dfb36bec7a1ced572840fba97d3e
Utpal Sonowal <132993823+Utpalsonowal7@users.noreply.github.com>
2025-06-02 01:07:41 +0000

    Sting and StirngBuffer class

--- str.java
+++ str.java
@@ -1,7 +1,9 @@
 class str {
     public static void main(String[] args) {
+        //string class
         String str1 = "utpal";
         String str2 = new String("utpal");
+
 
         System.out.println(str1 == str2);
         System.out.println(str1.equals(str2));
@@ -14,5 +16,11 @@ class str {
         System.out.println(str1.toUpperCase());
         System.out.println(str1.replace('a', 'o'));
         System.out.println(str1.contains("a"));
+
+        //StringBuffer class
+        StringBuffer str3 = new StringBuffer("Hello,");
+
+        System.out.println(str3.append(" world! "));
+        System.out.println(str3.insert( 0, " world! "));
     }
 }
